452104171fee41ab3cfbe1ec50dca72dd33f0b06
Daniel Claes <daniel.claes@liverpool.ac.uk>
2015-02-26 15:19:16 +0000

    move to new repo

--- get_file.sh
+++ get_file.sh
@@ -3,18 +3,18 @@
 
 test()
 {
-    wget --timeout=10 --spider swarmlab.dyndns.org > /dev/null 2>&1
+    wget --timeout=10 --spider www.github.com > /dev/null 2>&1
     if [ "$?" -eq 0 ]
     then
         echo Network connection now initialised # do something
         rm sync.sh > /dev/null 2>&1
-	wget -q "http://swarmlab.dyndns.org/tbsetup/tbsetup/raw-file/tip/sync.sh"
+	wget -q "https://raw.githubusercontent.com/smARTLab-liv/tbsetup/master/sync.sh"
 	chmod +x sync.sh
 	./sync.sh
         exit
     else
         echo "No network connection available, skipping updates"
-           sleep 5
+        sleep 5
     fi
 }
 
