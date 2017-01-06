3f33c2ec05d37760b433ff04d4fe793c3c55efa3
Daniel Claes <daniel.claes@liverpool.ac.uk>
2017-01-06 04:19:28 +0000

    lock

--- get_file.sh
+++ get_file.sh
@@ -6,12 +6,15 @@ test()
     wget --timeout=10 --spider www.github.com > /dev/null 2>&1
     if [ "$?" -eq 0 ]
     then
-        echo Network connection now initialised # do something
 	      cd /home/turtlebot/scripts
+        touch lock.file
+        echo Network connection now initialised # do something
+
         rm sync.sh > /dev/null 2>&1
 	      wget -q "https://raw.githubusercontent.com/smARTLab-liv/tbsetup/master/sync.sh"
 	      chmod +x sync.sh
 	      ./sync.sh > /home/turtlebot/scripts/sync.log 2>&1
+        rm lock.file > /dev/null 2>&1
         exit
     else
         echo "No network connection available, skipping updates"
@@ -19,4 +22,8 @@ test()
     fi
 }
 
+cd /home/turtlebot/scripts
+if [ -e lock.file ]
+   echo "File locked, other process still running? otherwise remove manually"
+   exit
 test
